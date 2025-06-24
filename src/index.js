979544999b114f4123353662238d7cae18326d38
Sanjana Sanjeevan <sanjanasanjeevan20@gmail.com>
2025-06-24 10:22:39 +0530

    Fix: Update redirect URLs for live deployment

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ root.render(
   <React.StrictMode>
     <AuthProvider
       config={{
-        signInRedirectURL: "http://localhost:3000",
-        signOutRedirectURL: "http://localhost:3000",
+        signInRedirectURL: "https://8375e0da-b929-4b3a-a6a1-cf62518814c0.e1-eu-north-azure.choreoapps.dev",
+        signOutRedirectURL: "https://8375e0da-b929-4b3a-a6a1-cf62518814c0.e1-eu-north-azure.choreoapps.dev",
         clientID: "YAL1gx453cfZ2acg240aCyDmgOEa", 
         baseUrl: "https://api.eu.asgardeo.io/t/locallink1", 
         scope: ["openid", "profile", "groups", "email"] // Add 'groups' to get roles later
